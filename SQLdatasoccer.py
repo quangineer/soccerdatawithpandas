51744313c3e03311f70a5478d36bc1bde4862b8f
Quan Nguyen <quangineer@gmail.com>
2019-05-07 14:10:07 -0700

    SQL in Pandas in Python

---
+++ SQLdatasoccer.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+
+filedata = 'data.csv'
+
+#Read File :
+data = pd.read_csv(filedata)
+
+#Make an object out of an object:
+data_Age = data["Age"]
+
+#Make a list from object of data["Age"] from object of data:
+# data_Age = data["Age"].tolist()
+
+#Use python to find out how many players' age equal and over 30:
+# A = []
+# for i in range(len(data_Age)):
+#     if data_Age[i] >= 30:
+#         A.append(data_Age[i])    
+# print (len(A))
+
+#Shorter verion of the operation above:
+# B = 0
+# for i in range(len(data_Age)):
+#     if data_Age[i] >= 30:
+#         B += 1
+# print (B)
+
+#print how many columns and how many row in each column (does not count cell without any values)
+# print (data.count())
+
+#print how many row in columns Age
+# print (data_Age.count())
+
+#given the condition of Age >= 30, find out how many players in data file using SQL:
+#This SQL count include every columns counts 
+# print ((data[data.Age >= 30]).count())
+
+#Using SQL to count: First select only column Age (of table data) where Age >= 30, then start counting
+#For this SQL count, have to decide which column , which condition first to narrow the result, then using index of 
+#data file to start counting.
+print ((data[data.Age >= 30])["Age"].count())
